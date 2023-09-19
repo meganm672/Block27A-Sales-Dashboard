1af7623135c5ce28b1121e59d83f7bde71ce7747
meganm672 <meganm672@gmail.com>
2023-09-19 14:33:51 -0400

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Header from './components/header'
 import './App.css'
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Header/>
       <h1>Hello</h1>
     </>
   )
